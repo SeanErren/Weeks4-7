0d8830a7d3cab2275efdb6a6ced94a288f756986
Sean Erren <ErrenSean@gmail.com>
2026-02-04 10:32:01 -0500

    Added comments to affecting the timer slider value.

--- Assets/Lessons/Scripts/Timer.cs
+++ Assets/Lessons/Scripts/Timer.cs
@@ -17,6 +17,8 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //When using "Whole Numbers" in the componant the digits after the point get cut off meaning that it removing 0 every time and gets stuck.
+        //It's good practice to use a saparate variable to make sure that I don't lose the numbers after the dot and in general is good practice.
         timerSlider.value -= Time.deltaTime;
         if (timerSlider.value <= 0 )
         {
