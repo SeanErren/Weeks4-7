4b88a90892e510bf4b80aa0dd83aa9b877afad3a
Sean Erren <ErrenSean@gmail.com>
2026-02-04 02:20:06 -0500

    Added sounds and run them through the inspector as well as code.

--- Assets/Lessons/Scripts/HealthBar.cs
+++ Assets/Lessons/Scripts/HealthBar.cs
@@ -9,10 +9,19 @@ public class HealthBar : MonoBehaviour
     public float playerHealth = 3;
     public float damageDone = 0.5f;
 
+    public AudioSource audioSource; //The componant
+    public AudioClip hitSound; //The sound file itself
+    public AudioClip deathSound; //The sound file itself
+
+    //AudioSource.Play() plays the clip in the AudioSource by starting and stopping it.
+    //AudioSource.PlayOneShot(clip) will stack the clips on top of each other, allowing for overlapping layers of sound (up to 32).
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = GetComponent<SpriteRenderer>();
+
+        //Setting the max health value and then filling up the bar
         healthBar.maxValue = playerHealth;
         healthBar.value = healthBar.maxValue;
     }
@@ -36,7 +45,20 @@ public class HealthBar : MonoBehaviour
     void loseHealth()
     {
         if (healthBar.value > 0)
+        {
             healthBar.value -= damageDone;
+            //Set the specific clip the audio source uses
+            audioSource.clip = hitSound;
+            //Play the clip
+            audioSource.Play();
+        }
+        if (healthBar.value == 0) //If the player just died
+        {
+            //Set the specific clip the audio source uses
+            audioSource.clip = deathSound;
+            //Play the clip
+            audioSource.Play();
+        }
         if (healthBar.value <= 0)
             gameObject.SetActive(false);
     }
