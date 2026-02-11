45f547c04b01740687de41fa119891330a13b230
Sean Erren <ErrenSean@gmail.com>
2026-02-11 09:56:05 -0500

    Added player movement.

--- Assets/Lessons/Frogger/Scripts/PlayerMovement.cs
+++ Assets/Lessons/Frogger/Scripts/PlayerMovement.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Direction direction = Direction.NONE;
+    float speedPerSec = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,49 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Set the movement direction
+        if (Keyboard.current.wKey.isPressed)
+        {
+            direction = Direction.UP;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            direction = Direction.DOWN;
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            direction = Direction.LEFT;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            direction = Direction.RIGHT;
+        }
+        else
+        {
+            direction = Direction.NONE;
+        }
+
+        //Move the player
+        switch (direction)
+        {
+            case Direction.NONE: break;
+            case Direction.UP:
+                transform.position += new Vector3(0,speedPerSec * Time.deltaTime, 0);
+                break;
+            case Direction.DOWN:
+                transform.position -= new Vector3(0, speedPerSec * Time.deltaTime, 0);
+                break;
+            case Direction.LEFT:
+                transform.position -= new Vector3(speedPerSec * Time.deltaTime, 0, 0);
+                break;
+            case Direction.RIGHT:
+                transform.position += new Vector3(speedPerSec * Time.deltaTime, 0, 0);
+                break;
+        }
     }
+}
+
+enum Direction
+{
+    NONE, UP, DOWN, LEFT, RIGHT
 }
